4a50caae0f9742783969997e48a1359b67645838
Qt3s <atsalper25@gmail.com>
2026-02-26 14:15:56 +0300

    fix(android): update MainApplication to extend MultiDexApplication

--- plugins/withAndroidFix.js
+++ plugins/withAndroidFix.js
@@ -1,52 +1,26 @@
-const { withDangerousMod } = require('@expo/config-plugins');
-const fs = require('fs');
-const path = require('path');
+const { withMainApplication } = require('@expo/config-plugins');
 
 module.exports = function withAndroidFix(config) {
-    return withDangerousMod(config, [
-        'android',
-        async (config) => {
-            const projectRoot = config.modRequest.projectRoot;
+    return withMainApplication(config, (config) => {
+        let contents = config.modResults.contents;
 
-            const mainApplicationPath = path.join(
-                projectRoot,
-                'android/app/src/main/java/com/qtes34/yokdilapp/MainApplication.kt'
+        // MultiDexApplication import ekle
+        if (!contents.includes('androidx.multidex.MultiDexApplication')) {
+            contents = contents.replace(
+                'import expo.modules.ReactActivityDelegateWrapper',
+                'import androidx.multidex.MultiDexApplication\nimport expo.modules.ReactActivityDelegateWrapper'
             );
+        }
 
-            const mainActivityPath = path.join(
-                projectRoot,
-                'android/app/src/main/java/com/qtes34/yokdilapp/MainActivity.kt'
+        // Application -> MultiDexApplication
+        if (!contents.includes('MultiDexApplication()')) {
+            contents = contents.replace(
+                'class MainApplication : Application(), ReactApplication {',
+                'class MainApplication : MultiDexApplication(), ReactApplication {'
             );
+        }
 
-            const replaceBuildConfigArgs = (content) => {
-                let newContent = content
-                    .replace(/BuildConfig\.IS_NEW_ARCHITECTURE_ENABLED/g, 'true')
-                    .replace(/BuildConfig\.DEBUG/g, 'false')
-                    .replace(/BuildConfig\.IS_HERMES_ENABLED/g, 'true')
-                    .replace(/BuildConfig\.REACT_NATIVE_RELEASE_LEVEL/g, '"stable"')
-                    .replace(/BuildConfig\.BUILD_TYPE/g, '"release"');
-
-                // Ensure BuildConfig is imported to avoid ClassNotFoundException
-                if (!newContent.includes('import com.qtes34.yokdilapp.BuildConfig')) {
-                    newContent = newContent.replace('package com.qtes34.yokdilapp', 'package com.qtes34.yokdilapp\n\nimport com.qtes34.yokdilapp.BuildConfig\n');
-                }
-
-                return newContent;
-            };
-
-            if (fs.existsSync(mainApplicationPath)) {
-                let content = fs.readFileSync(mainApplicationPath, 'utf8');
-                content = replaceBuildConfigArgs(content);
-                fs.writeFileSync(mainApplicationPath, content, 'utf8');
-            }
-
-            if (fs.existsSync(mainActivityPath)) {
-                let content = fs.readFileSync(mainActivityPath, 'utf8');
-                content = replaceBuildConfigArgs(content);
-                fs.writeFileSync(mainActivityPath, content, 'utf8');
-            }
-
-            return config;
-        },
-    ]);
+        config.modResults.contents = contents;
+        return config;
+    });
 };
