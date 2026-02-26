f25b8ba2208a803f309ec53b40d6a84cda8a7376
Qt3s <atsalper25@gmail.com>
2026-02-26 14:47:59 +0300

    fix(android): update MultiDex fix in withAndroidFix.js plugin

--- plugins/withAndroidFix.js
+++ plugins/withAndroidFix.js
@@ -1,26 +1,44 @@
-const { withMainApplication } = require('@expo/config-plugins');
+const { withAppBuildGradle, withMainApplication } = require('@expo/config-plugins');
 
 module.exports = function withAndroidFix(config) {
-    return withMainApplication(config, (config) => {
+    // 1. build.gradle'a multidex bağımlılığını ekle
+    config = withAppBuildGradle(config, (config) => {
         let contents = config.modResults.contents;
 
-        // MultiDexApplication import ekle
-        if (!contents.includes('androidx.multidex.MultiDexApplication')) {
+        if (!contents.includes('androidx.multidex:multidex')) {
             contents = contents.replace(
-                'import expo.modules.ReactActivityDelegateWrapper',
-                'import androidx.multidex.MultiDexApplication\nimport expo.modules.ReactActivityDelegateWrapper'
-            );
-        }
-
-        // Application -> MultiDexApplication
-        if (!contents.includes('MultiDexApplication()')) {
-            contents = contents.replace(
-                'class MainApplication : Application(), ReactApplication {',
-                'class MainApplication : MultiDexApplication(), ReactApplication {'
+                /dependencies\s*\{/,
+                'dependencies {\n    implementation("androidx.multidex:multidex:2.0.1")'
             );
         }
 
         config.modResults.contents = contents;
         return config;
     });
+
+    // 2. MainApplication.kt dosyasını düzelt
+    config = withMainApplication(config, (config) => {
+        let contents = config.modResults.contents;
+
+        // MultiDexApplication import ekle (dosyanın en başına)
+        if (!contents.includes('import androidx.multidex.MultiDexApplication')) {
+            contents = contents.replace(
+                /^(package .+\n)/m,
+                '$1\nimport androidx.multidex.MultiDexApplication\n'
+            );
+        }
+
+        // Application -> MultiDexApplication
+        if (!contents.includes('MultiDexApplication()')) {
+            contents = contents.replace(
+                /class MainApplication : Application\(\)/,
+                'class MainApplication : MultiDexApplication()'
+            );
+        }
+
+        config.modResults.contents = contents;
+        return config;
+    });
+
+    return config;
 };
